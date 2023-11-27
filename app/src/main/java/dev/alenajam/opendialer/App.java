cd46dba23980b3da23cd304b2846c80ae13058bb
Alen Ajam <hello@alenajam.dev>
2023-11-27 22:16:08 +0100

    chore: add java core library desugaring (#22)
    * chore: remove com.jakewharton.threetenabp:threetenabp dependency
    
    * chore: add java core library desugaring

--- app/src/main/java/dev/alenajam/opendialer/App.java
+++ app/src/main/java/dev/alenajam/opendialer/App.java
@@ -3,8 +3,6 @@ package dev.alenajam.opendialer;
 import android.app.Application;
 import android.content.SharedPreferences;
 
-import com.jakewharton.threetenabp.AndroidThreeTen;
-
 import dagger.hilt.android.HiltAndroidApp;
 import dev.alenajam.opendialer.helper.NotificationHelper;
 import dev.alenajam.opendialer.helper.SharedPreferenceHelper;
@@ -16,7 +14,6 @@ public class App extends Application {
   public void onCreate() {
     super.onCreate();
     NotificationHelper.setupNotificationChannels(this);
-    AndroidThreeTen.init(this);
     SharedPreferenceHelper.init(this);
   }
 
