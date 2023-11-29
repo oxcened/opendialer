81f1d764959fb7e1125b5cc3479efb25d2bfc2fa
Alen Ajam <alen.ajam@live.com>
2023-11-30 00:33:41 +0100

    fix: build issues

--- app/src/main/java/dev/alenajam/opendialer/core/exception/Failure.kt
+++ app/src/main/java/dev/alenajam/opendialer/core/exception/Failure.kt
@@ -1,7 +1,5 @@
 package dev.alenajam.opendialer.core.exception
 
-import dev.alenajam.opendialer.core.common.exception.Failure
-
 sealed class Failure(var source: String? = null) {
   class GenericFailure(val throwable: Throwable? = null, source: String? = null) : Failure(source)
   object NoData : Failure()
