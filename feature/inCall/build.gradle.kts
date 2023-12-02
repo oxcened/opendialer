14b77ae3744f0f845d335e715c9e6b081adc8c9b
Alen Ajam <alen.ajam@live.com>
2023-12-02 01:26:18 +0100

    fix: remove module impl

--- feature/inCall/build.gradle.kts
+++ feature/inCall/build.gradle.kts
@@ -37,7 +37,6 @@ android {
 
 dependencies {
   implementation(project(":core:common"))
-  implementation(project(":data:inCall"))
 
   implementation("androidx.core:core-ktx:1.9.0")
   implementation("androidx.appcompat:appcompat:1.6.1")
