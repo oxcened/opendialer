cfc758e5b513b3432efe28fc12575cbd11586b7f
Alen Ajam <alen@fiscozen.it>
2024-10-07 17:14:41 +0200

    feat(searchContacts): new compose screen

--- core/common/build.gradle.kts
+++ core/common/build.gradle.kts
@@ -1,58 +1,61 @@
 plugins {
-  id("com.android.library")
-  id("org.jetbrains.kotlin.android")
-  id("kotlin-kapt")
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
 }
 
 android {
-  namespace = "dev.alenajam.opendialer.core.common"
-  compileSdk = 34
+    namespace = "dev.alenajam.opendialer.core.common"
+    compileSdk = 34
 
-  defaultConfig {
-    minSdk = 24
+    defaultConfig {
+        minSdk = 24
 
-    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    consumerProguardFiles("consumer-rules.pro")
-  }
-
-  buildTypes {
-    release {
-      isMinifyEnabled = false
-      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
-  }
-  buildFeatures {
-    viewBinding = true
-  }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
-  implementation("androidx.core:core-ktx:1.9.0")
-  implementation("androidx.appcompat:appcompat:1.6.1")
-  implementation("com.google.android.material:material:1.10.0")
-  testImplementation("junit:junit:4.13.2")
-  androidTestImplementation("androidx.test.ext:junit:1.1.5")
-  androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.10.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-  implementation("com.squareup.picasso:picasso:2.71828")
-  implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
-  implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
-  implementation("androidx.preference:preference-ktx:1.2.1")
-  implementation("com.google.code.gson:gson:2.9.0")
+    implementation("com.squareup.picasso:picasso:2.71828")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
+    implementation("androidx.preference:preference-ktx:1.2.1")
+    implementation("com.google.code.gson:gson:2.9.0")
 
-  val composeBom = platform("androidx.compose:compose-bom:2024.09.03")
-  implementation(composeBom)
-  androidTestImplementation(composeBom)
-  implementation("androidx.compose.material3:material3")
-  implementation("androidx.compose.ui:ui-tooling-preview")
-  debugImplementation("androidx.compose.ui:ui-tooling")
-  androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-  debugImplementation("androidx.compose.ui:ui-test-manifest")
-  implementation("androidx.compose.material:material-icons-extended")
-  implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
-  implementation("androidx.compose.runtime:runtime-livedata")
+    val composeBom = platform("androidx.compose:compose-bom:2024.09.03")
+    implementation(composeBom)
+    androidTestImplementation(composeBom)
+    implementation(libs.androidx.material3)
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation("androidx.compose.material:material-icons-extended")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    implementation("androidx.compose.runtime:runtime-livedata")
 }
 
 kotlin {
-  jvmToolchain(17)
+    jvmToolchain(17)
 }
--- feature/contactsSearch/src/main/java/dev/alenajam/opendialer/feature/contactsSearch/SearchContactsViewModel.kt
+++ feature/contactsSearch/src/main/java/dev/alenajam/opendialer/feature/contactsSearch/SearchContactsViewModel.kt
@@ -2,50 +2,81 @@ package dev.alenajam.opendialer.feature.contactsSearch
 
 import android.app.Activity
 import android.app.Application
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dev.alenajam.opendialer.core.aosp.SmartDialNameMatcher
 import dev.alenajam.opendialer.core.common.CommonUtils
+import dev.alenajam.opendialer.core.common.PermissionUtils
 import dev.alenajam.opendialer.data.contactsSearch.DialerSearchContact
 import dev.alenajam.opendialer.data.contactsSearch.DialerSearchContactEntity
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class SearchContactsViewModel
 @Inject constructor(
-  private val app: Application,
-  private val searchContactsUseCase: SearchContacts,
-  private val searchContactsDialpadUseCase: SearchContactsDialpad
+    private val app: Application,
+    private val searchContactsUseCase: SearchContacts,
+    private val searchContactsDialpadUseCase: SearchContactsDialpad
 ) : ViewModel() {
-  val result: MutableLiveData<Result> = MutableLiveData()
+    private val _result = MutableStateFlow<Result?>(null)
+    val result: StateFlow<Result?> = _result
+    private val _hasRuntimePermission = MutableStateFlow(false)
+    val hasRuntimePermission: StateFlow<Boolean> = _hasRuntimePermission
+    private val _hasCallRuntimePermission = MutableStateFlow(false)
+    private val hasCallRuntimePermission: StateFlow<Boolean> = _hasCallRuntimePermission
 
-  fun searchContacts(query: String) =
-    searchContactsUseCase(viewModelScope, SearchContactsParams(app.contentResolver, query)) {
-      it.fold({}) { res -> handleResult(query, res) }
+    init {
+        _hasRuntimePermission.value = PermissionUtils.hasSearchPermission(app)
+        _hasCallRuntimePermission.value = PermissionUtils.hasMakeCallPermission(app)
     }
 
-  fun searchContactsDialpad(query: String) = searchContactsDialpadUseCase(
-    viewModelScope,
-    SearchContactsDialpadParams(app.contentResolver, query)
-  ) {
-    it.fold({}) { res -> handleResult(query, res) }
-  }
+    fun handleRuntimePermissionGranted(query: String) {
+        _hasRuntimePermission.value = true
+        searchContactsByDialpad(query)
+    }
 
-  private fun handleResult(query: String, contacts: List<DialerSearchContactEntity>) {
-    result.postValue(Result(query, DialerSearchContact.mapList(contacts)))
-  }
+    fun handleCallRuntimePermissionGranted() {
+        _hasCallRuntimePermission.value = true
+    }
 
-  fun makeCall(activity: Activity, number: String) = CommonUtils.makeCall(activity, number)
-  fun sendMessage(activity: Activity, number: String) = CommonUtils.makeSms(activity, number)
-  fun createContact(activity: Activity, number: String) =
-    CommonUtils.createContact(activity, number)
+    fun searchContacts(query: String) {
+        if (!hasRuntimePermission.value) return
+        searchContactsUseCase(viewModelScope, SearchContactsParams(app.contentResolver, query)) {
+            it.fold({}) { res -> handleResult(query, res) }
+        }
+    }
 
-  fun addToContact(activity: Activity, number: String) =
-    CommonUtils.addContactAsExisting(activity, number)
+    fun searchContactsByDialpad(query: String) {
+        if (!hasRuntimePermission.value) return
+        searchContactsDialpadUseCase(
+            viewModelScope,
+            SearchContactsDialpadParams(app.contentResolver, query)
+        ) {
+            it.fold({}) { res -> handleResult(SmartDialNameMatcher.normalizeNumber(app, query), res) }
+        }
+    }
 
-  class Result(
-    val query: String,
-    val contacts: List<DialerSearchContact>
-  )
+    private fun handleResult(query: String, contacts: List<DialerSearchContactEntity>) {
+        _result.value = Result(query, DialerSearchContact.mapList(contacts))
+    }
+
+    fun makeCall(activity: Activity, number: String): Boolean {
+        if (!hasCallRuntimePermission.value) return false
+        CommonUtils.makeCall(activity, number)
+        return true
+    }
+    fun sendMessage(activity: Activity, number: String) = CommonUtils.makeSms(activity, number)
+    fun createContact(activity: Activity, number: String) =
+        CommonUtils.createContact(activity, number)
+
+    fun addToContact(activity: Activity, number: String) =
+        CommonUtils.addContactAsExisting(activity, number)
+
+    class Result(
+        val query: String,
+        val contacts: List<DialerSearchContact>
+    )
 }
